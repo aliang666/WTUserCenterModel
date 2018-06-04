abceb085bf1ea4d21bf586d419250a997d6e61c3
elji <elji@iflytek.com>
2018-06-04 20:37:29 +0800

    个人中心
    个人中心

---
+++ WTUserCenterModel/WTUserCenterViewController.m
@@ -0,0 +1,21 @@
+//
+//  WTUserCenterViewController.m
+//  WTEmptyProject
+//
+//  Created by 计恩良 on 2018/5/27.
+//  Copyright © 2018年 计恩良. All rights reserved.
+//
+
+#import "WTUserCenterViewController.h"
+
+@interface WTUserCenterViewController ()
+
+@end
+
+@implementation WTUserCenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navBar.hidden = YES;
+}
+@end
